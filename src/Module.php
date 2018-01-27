3b553e4f8feaba2e86e25bd837e0c4b6599651ff
Kaj Ström <kaj.strom@kapsi.fi>
2018-01-27 13:05:10 +0200

    PHPDoc comments for Module interface

--- src/Module.php
+++ src/Module.php
@@ -5,12 +5,35 @@ namespace KajStrom\DependencyConstraints;
 
 interface Module
 {
+    /**
+     * Returns the qualified name of this module.
+     *
+     * @return string
+     */
     public function getName() : string;
 
+    /**
+     * Checks if this module depends on the specified module.
+     *
+     * @param string $module
+     * @return bool
+     */
     public function dependsOnModule(string $module) : bool;
 
+    /**
+     * Checks if this module depends on the specified class/function/constant.
+     *
+     * @param string $fqn
+     * @return bool
+     */
     public function hasDependencyOn(string $fqn) : bool;
 
+    /**
+     * Checks if this module is the given module.
+     *
+     * @param string $module
+     * @return bool
+     */
     public function is(string $module) : bool;
 
     /**
